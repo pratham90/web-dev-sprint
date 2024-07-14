34f26361ba2922ce110878d5bb5cc99daead316c
pratham rathore <prathamrathore2004@gmail.com>
2024-07-14 18:16:36 +0530

    first commit

---
+++ bhj.js
@@ -0,0 +1,28 @@
+gsap.from("nav",{
+    y:-1000,
+    delay:0.5,
+    duration:3,
+    rotate:180
+})
+gsap.from(".main-text",{
+    x:-1000,
+    delay:1,
+    y:-1000,
+    duration:3
+})
+gsap.from(".arrow",{
+    y:-400,
+    delay:1,
+    duration:2,
+    rotate:200
+})
+gsap.from("nav ul",{
+    y:-2000,
+    delay:3,
+    duration:1.5
+})
+gsap.from(".nav-items img",{
+    y:-100,
+    delay:3,
+    duration:1.5
+})
